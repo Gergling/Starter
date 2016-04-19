ba82e2fea4e20729875f8305d9d44de8f2c86b6d
gergling <gregmichaeldavies@gmail.com>
2016-04-19 16:36:50 +0100

    Converted indentation to spaces.

--- src/client/toolkit/absolute-tiles/factory.js
+++ src/client/toolkit/absolute-tiles/factory.js
@@ -1,31 +1,31 @@
 angular.module('gdToolkitAbsoluteTiles').factory('gdToolkitAbsoluteTilesFactory', function () {
-	var Tile = function () {
+    var Tile = function () {
 
-	};
-	var TileSet = function () {
-		var tiles = [];
+    };
+    var TileSet = function () {
+        var tiles = [];
 
-		this.tile = function (x, y) {
-			if (typeof y === 'number') {
-				var tile = tiles.filter(function (tile) {
-					return tile.x() === x && tile.y() === y;
-				})[0];
-				if (!tile) {
-					tile = new Tile();
-				}
-				tiles.push(tile);
-			}
-		};
-	};
+        this.tile = function (x, y) {
+            if (typeof y === 'number') {
+                var tile = tiles.filter(function (tile) {
+                    return tile.x() === x && tile.y() === y;
+                })[0];
+                if (!tile) {
+                    tile = new Tile();
+                }
+                tiles.push(tile);
+            }
+        };
+    };
 
-	return function (options) {
-		if (options === 'test') {
-			return {
-				TileSet: TileSet,
-				Tile: Tile
-			};
-		} else {
-			return new TileSet();
-		}
-	};
+    return function (options) {
+        if (options === 'test') {
+            return {
+                TileSet: TileSet,
+                Tile: Tile
+            };
+        } else {
+            return new TileSet();
+        }
+    };
 });
